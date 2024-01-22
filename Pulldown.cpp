c2335a1c49df22d05313bca8b419d636b14a8e9d
eight <hgrmtr8@gmail.com>
2024-01-22 22:46:39 +0900

    コード整形

--- Pulldown.cpp
+++ Pulldown.cpp
@@ -4,158 +4,157 @@
 namespace CustomizedSimpleGUI
 {
 
-Pulldown* Pulldown::currentOpeningListBox = nullptr;
+	Pulldown* Pulldown::currentOpeningListBox = nullptr;
 
-void Pulldown::closeCurrentOpeningListBox()
-{
-	if (Pulldown::currentOpeningListBox != nullptr)
+	void Pulldown::closeCurrentOpeningListBox()
 	{
-		Pulldown::currentOpeningListBox->m_isOpen = false;
-		Pulldown::currentOpeningListBox = nullptr;
-	}
-}
-
-// これを使う場合はシーンのコンストラクタで必ずPulldown::setItems()を実行すること
-Pulldown::Pulldown(const Font font, const Vec2& displayPos, const double width, const int32 listCount)
-	: m_font(font)
-	, m_displayPos(displayPos)
-	, m_listCount(listCount)
-{
-	m_isOpen = false;
-	m_state.selectedItemIndex = 0;
-	m_fontHeight = m_font.height();
-	m_displayRegion = { m_displayPos, Max(width, 40.0), font.height() + (ListBoxFrameThickness * 2) };
-	double height = (m_fontHeight + (ListBoxFrameThickness * 2)) * m_listCount;
-	m_maxLines = (static_cast<int32>(height) - (ListBoxFrameThickness * 2)) / m_fontHeight;
-	m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
-	hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
-	Optional<size_t> oldState = m_state.selectedItemIndex;
-	m_listRegion = {
-		m_displayRegion.pos.movedBy(0, m_displayRegion.h),
-		width,
-		Max<double>(height, font.height() + (ListBoxFrameThickness * 2))
-	};
-	m_listPos = m_listRegion.pos.movedBy(ListBoxFrameThickness, ListBoxFrameThickness);
-	itemWidth = static_cast<int32>(width - (ListBoxFrameThickness * 2) - (hasScrollBar ? ScrollBarWidth : 0));
-}
-
-Pulldown::Pulldown(ListBoxState state, const Font font, const Vec2& displayPos, const double width, const int32 listCount)
-	: m_state(state)
-	, m_font(font)
-	, m_displayPos(displayPos)
-	, m_listCount(listCount)
-{
-	m_isOpen = false;
-	m_state.selectedItemIndex = 0;
-	m_fontHeight = m_font.height();
-	m_displayRegion = { m_displayPos, Max(width, 40.0), font.height() + (ListBoxFrameThickness * 2) };
-	double height = (m_fontHeight + (ListBoxFrameThickness * 2)) * m_listCount;
-	m_maxLines = (static_cast<int32>(height) - (ListBoxFrameThickness * 2)) / m_fontHeight;
-	m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
-	hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
-	Optional<size_t> oldState = m_state.selectedItemIndex;
-	m_listRegion = {
-		m_displayRegion.pos.movedBy(0, m_displayRegion.h),
-		width,
-		Max<double>(height, font.height() + (ListBoxFrameThickness * 2))
-	};
-	m_listPos = m_listRegion.pos.movedBy(ListBoxFrameThickness, ListBoxFrameThickness);
-	itemWidth = static_cast<int32>(width - (ListBoxFrameThickness * 2) - (hasScrollBar ? ScrollBarWidth : 0));
-}
-
-void Pulldown::emplace_back(String item)
-{
-	m_state.items.emplace_back(item);
-	m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
-	hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
-}
-
-void Pulldown::setItems(const Array<String>& items)
-{
-	m_state.items = items;
-	m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
-	hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
-}
-
-void Pulldown::setIndex(const size_t index)
-{
-	m_state.selectedItemIndex = index;
-}
-
-void Pulldown::setIndexFromItem(const String& item)
-{
-	for (size_t i = 0; i < m_state.items.size(); ++i)
-	{
-		if (m_state.items[i] == item)
+		if (Pulldown::currentOpeningListBox != nullptr)
 		{
-			m_state.selectedItemIndex = i;
-			return;
+			Pulldown::currentOpeningListBox->m_isOpen = false;
+			Pulldown::currentOpeningListBox = nullptr;
 		}
 	}
-}
 
-size_t Pulldown::getSelectedIndex() const
-{
-	return *m_state.selectedItemIndex;
-}
-
-String Pulldown::getSelectedItem() const
-{
-	if (m_state.selectedItemIndex)
+	// これを使う場合はシーンのコンストラクタで必ずPulldown::setItems()を実行すること
+	Pulldown::Pulldown(const Font font, const Vec2& displayPos, const double width, const int32 listCount)
+		: m_font(font)
+		, m_displayPos(displayPos)
+		, m_listCount(listCount)
 	{
-		return m_state.items[*m_state.selectedItemIndex];
+		m_isOpen = false;
+		m_state.selectedItemIndex = 0;
+		m_fontHeight = m_font.height();
+		m_displayRegion = { m_displayPos, Max(width, 40.0), font.height() + (ListBoxFrameThickness * 2) };
+		double height = (m_fontHeight + (ListBoxFrameThickness * 2)) * m_listCount;
+		m_maxLines = (static_cast<int32>(height) - (ListBoxFrameThickness * 2)) / m_fontHeight;
+		m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
+		hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
+		Optional<size_t> oldState = m_state.selectedItemIndex;
+		m_listRegion = {
+			m_displayRegion.pos.movedBy(0, m_displayRegion.h),
+			width,
+			Max<double>(height, font.height() + (ListBoxFrameThickness * 2))
+		};
+		m_listPos = m_listRegion.pos.movedBy(ListBoxFrameThickness, ListBoxFrameThickness);
+		itemWidth = static_cast<int32>(width - (ListBoxFrameThickness * 2) - (hasScrollBar ? ScrollBarWidth : 0));
 	}
-	else
+
+	Pulldown::Pulldown(ListBoxState state, const Font font, const Vec2& displayPos, const double width, const int32 listCount)
+		: m_state(state)
+		, m_font(font)
+		, m_displayPos(displayPos)
+		, m_listCount(listCount)
 	{
-		return String();
+		m_isOpen = false;
+		m_state.selectedItemIndex = 0;
+		m_fontHeight = m_font.height();
+		m_displayRegion = { m_displayPos, Max(width, 40.0), font.height() + (ListBoxFrameThickness * 2) };
+		double height = (m_fontHeight + (ListBoxFrameThickness * 2)) * m_listCount;
+		m_maxLines = (static_cast<int32>(height) - (ListBoxFrameThickness * 2)) / m_fontHeight;
+		m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
+		hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
+		Optional<size_t> oldState = m_state.selectedItemIndex;
+		m_listRegion = {
+			m_displayRegion.pos.movedBy(0, m_displayRegion.h),
+			width,
+			Max<double>(height, font.height() + (ListBoxFrameThickness * 2))
+		};
+		m_listPos = m_listRegion.pos.movedBy(ListBoxFrameThickness, ListBoxFrameThickness);
+		itemWidth = static_cast<int32>(width - (ListBoxFrameThickness * 2) - (hasScrollBar ? ScrollBarWidth : 0));
 	}
-}
 
-bool Pulldown::getIsOpen() const
-{
-	return m_isOpen;
-}
-
-void Pulldown::update()
-{
-	// クリックしたとき
-	if (m_displayRegion.leftClicked())
+	void Pulldown::emplace_back(String item)
 	{
-		// もしほかのリストボックスが開いていたら更新しない
-		if (currentOpeningListBox != nullptr && currentOpeningListBox != this)
+		m_state.items.emplace_back(item);
+		m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
+		hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
+	}
+
+	void Pulldown::setItems(const Array<String>& items)
+	{
+		m_state.items = items;
+		m_lines = Min<int32>(m_maxLines, static_cast<int32>(m_state.items.size()));
+		hasScrollBar = (m_maxLines < static_cast<int32>(m_state.items.size()));
+	}
+
+	void Pulldown::setIndex(const size_t index)
+	{
+		m_state.selectedItemIndex = index;
+	}
+
+	void Pulldown::setIndexFromItem(const String& item)
+	{
+		for (size_t i = 0; i < m_state.items.size(); ++i)
 		{
-			return;
+			if (m_state.items[i] == item)
+			{
+				m_state.selectedItemIndex = i;
+				return;
+			}
 		}
-		m_isOpen = !m_isOpen;
-		if (m_isOpen)
+	}
+
+	size_t Pulldown::getSelectedIndex() const
+	{
+		return *m_state.selectedItemIndex;
+	}
+
+	String Pulldown::getSelectedItem() const
+	{
+		if (m_state.selectedItemIndex)
 		{
-			currentOpeningListBox = this;
+			return m_state.items[*m_state.selectedItemIndex];
 		}
 		else
 		{
-			currentOpeningListBox = nullptr;
+			return String();
 		}
 	}
 
-	if (not m_isOpen) return;
-
-	// リストボックスの更新
+	bool Pulldown::getIsOpen() const
 	{
+		return m_isOpen;
+	}
 
-			for (int32 i = 0; i < m_lines; ++i)
+	void Pulldown::update()
+	{
+		// クリックしたとき
+		if (m_displayRegion.leftClicked())
+		{
+			// もしほかのリストボックスが開いていたら更新しない
+			if (currentOpeningListBox != nullptr && currentOpeningListBox != this)
 			{
-				const size_t itemIndex = (m_state.scroll + i);
-				const RectF itemRect(m_listPos.x + ListBoxFrameThickness, m_listPos.y + ListBoxFrameThickness + (m_fontHeight * i), itemWidth, m_fontHeight);
-
-				if (itemRect.leftClicked())
-				{
-					m_state.active = true;
-					m_state.selectedItemIndex = itemIndex;
-					currentOpeningListBox = nullptr;
-					m_isOpen = false;
-				}
+				return;
 			}
+			m_isOpen = !m_isOpen;
+			if (m_isOpen)
+			{
+				currentOpeningListBox = this;
+			}
+			else
+			{
+				currentOpeningListBox = nullptr;
+			}
+		}
 
+		if (not m_isOpen) return;
+
+
+		// リストボックスの更新
+
+		for (int32 i = 0; i < m_lines; ++i)
+		{
+			const size_t itemIndex = (m_state.scroll + i);
+			const RectF itemRect(m_listPos.x + ListBoxFrameThickness, m_listPos.y + ListBoxFrameThickness + (m_fontHeight * i), itemWidth, m_fontHeight);
+
+			if (itemRect.leftClicked())
+			{
+				m_state.active = true;
+				m_state.selectedItemIndex = itemIndex;
+				currentOpeningListBox = nullptr;
+				m_isOpen = false;
+			}
+		}
 
 		if (MouseL.down() && not m_listRegion.mouseOver())
 		{
@@ -250,114 +249,113 @@ void Pulldown::update()
 			m_state.wheel = 0.0;
 		}
 	}
-}
 
-void Pulldown::draw() const
-{
-	m_displayRegion.draw().drawFrame(DisplayRegionFrameThickness, 0, m_isOpen ? Palette::Orange : Palette::Gray);
-
-	m_font(m_state.items[*m_state.selectedItemIndex]).draw(Arg::leftCenter(m_displayRegion.leftCenter().x + 5, m_displayRegion.leftCenter().y), Palette::Black);
-
-	Triangle{ m_displayRegion.rightCenter().movedBy(-20, 0), (m_displayRegion.h / 3.0), 180_deg }.draw(Palette::Black);
-
-	if (not m_isOpen) return;
-
-	// リストボックスの描画
+	void Pulldown::draw() const
 	{
-		constexpr int32 PaddingLeft = 8;
-		constexpr int32 PaddingRight = 8;
+		m_displayRegion.draw().drawFrame(DisplayRegionFrameThickness, 0, m_isOpen ? Palette::Orange : Palette::Gray);
 
-		m_listRegion
-			.draw()
-			.drawFrame(1, 0, ColorF{ 0.5 });
+		m_font(m_state.items[*m_state.selectedItemIndex]).draw(Arg::leftCenter(m_displayRegion.leftCenter().x + 5, m_displayRegion.leftCenter().y), Palette::Black);
 
-		const int32 textMaxWidth = itemWidth - (PaddingLeft + PaddingRight);
-		const ColorF textColor = ActiveTextColor;
+		Triangle{ m_displayRegion.rightCenter().movedBy(-20, 0), (m_displayRegion.h / 3.0), 180_deg }.draw(Palette::Black);
 
-		for (int32 i = 0; i < m_lines; ++i)
+		if (not m_isOpen) return;
+
+		// リストボックスの描画
 		{
-			const size_t itemIndex = (m_state.scroll + i);
-			const bool selected = (itemIndex == m_state.selectedItemIndex);
-			const RectF itemRect(m_listPos.x + ListBoxFrameThickness, m_listPos.y + ListBoxFrameThickness + (m_fontHeight * i), itemWidth, m_fontHeight);
-			const RectF textRect(itemRect.pos.movedBy(PaddingLeft, 0), textMaxWidth, itemRect.h);
-			const String& text = m_state.items[itemIndex];
-			if (itemRect.mouseOver())
+			constexpr int32 PaddingLeft = 8;
+			constexpr int32 PaddingRight = 8;
+
+			m_listRegion
+				.draw()
+				.drawFrame(1, 0, ColorF{ 0.5 });
+
+			const int32 textMaxWidth = itemWidth - (PaddingLeft + PaddingRight);
+			const ColorF textColor = ActiveTextColor;
+
+			for (int32 i = 0; i < m_lines; ++i)
 			{
-				itemRect.draw(ColorF{ 0.85 });
-			}
-			if (selected)
-			{
-				const ColorF selectedItemBackgroundColor = ListBoxSelectedColor;
-
-				itemRect
-					.stretched(-1, 0, 0, -1)
-					.draw(selectedItemBackgroundColor);
-
-				m_font(text).draw(textRect, ColorF{ 1.0 });
-			}
-			else
-			{
-				m_font(text).draw(textRect, textColor);
-			}
-		}
-
-		if (hasScrollBar)
-		{
-			const RectF scrollBarArea(m_listPos.x + m_listRegion.w - (ListBoxFrameThickness + ScrollBarWidth), m_listPos.y + ListBoxFrameThickness, ScrollBarWidth, m_listRegion.h - (ListBoxFrameThickness * 2));
-			const RectF upButton(scrollBarArea.pos, ScrollBarWidth, ScrollBarWidth);
-			const RectF downButton(scrollBarArea.x, scrollBarArea.y + scrollBarArea.h - ScrollBarWidth, ScrollBarWidth, ScrollBarWidth);
-
-			scrollBarArea.draw(ColorF(0.85));
-
-			{
-				const bool pressed = ((not m_state.scrollBarGrabbed) && upButton.leftPressed());
-				const Vec2 c = upButton.center();
-
-				if (pressed)
+				const size_t itemIndex = (m_state.scroll + i);
+				const bool selected = (itemIndex == m_state.selectedItemIndex);
+				const RectF itemRect(m_listPos.x + ListBoxFrameThickness, m_listPos.y + ListBoxFrameThickness + (m_fontHeight * i), itemWidth, m_fontHeight);
+				const RectF textRect(itemRect.pos.movedBy(PaddingLeft, 0), textMaxWidth, itemRect.h);
+				const String& text = m_state.items[itemIndex];
+				if (itemRect.mouseOver())
 				{
-					upButton
-						.stretched(-1)
-						.draw(ColorF{ 0.33 });
+					itemRect.draw(ColorF{ 0.85 });
+				}
+				if (selected)
+				{
+					const ColorF selectedItemBackgroundColor = ListBoxSelectedColor;
+
+					itemRect
+						.stretched(-1, 0, 0, -1)
+						.draw(selectedItemBackgroundColor);
+
+					m_font(text).draw(textRect, ColorF{ 1.0 });
+				}
+				else
+				{
+					m_font(text).draw(textRect, textColor);
+				}
+			}
+
+			if (hasScrollBar)
+			{
+				const RectF scrollBarArea(m_listPos.x + m_listRegion.w - (ListBoxFrameThickness + ScrollBarWidth), m_listPos.y + ListBoxFrameThickness, ScrollBarWidth, m_listRegion.h - (ListBoxFrameThickness * 2));
+				const RectF upButton(scrollBarArea.pos, ScrollBarWidth, ScrollBarWidth);
+				const RectF downButton(scrollBarArea.x, scrollBarArea.y + scrollBarArea.h - ScrollBarWidth, ScrollBarWidth, ScrollBarWidth);
+
+				scrollBarArea.draw(ColorF(0.85));
+
+				{
+					const bool pressed = ((not m_state.scrollBarGrabbed) && upButton.leftPressed());
+					const Vec2 c = upButton.center();
+
+					if (pressed)
+					{
+						upButton
+							.stretched(-1)
+							.draw(ColorF{ 0.33 });
+					}
+
+					Triangle{ c.movedBy(0, -ScrollBarWidth * 0.22),
+						c.movedBy(ScrollBarWidth * 0.25, ScrollBarWidth * 0.08),
+						c.movedBy(-ScrollBarWidth * 0.25, ScrollBarWidth * 0.08) }
+					.draw(pressed ? ColorF{ 0.85 } : ColorF{ 0.33 });
 				}
 
-				Triangle{ c.movedBy(0, -ScrollBarWidth * 0.22),
-					c.movedBy(ScrollBarWidth * 0.25, ScrollBarWidth * 0.08),
-					c.movedBy(-ScrollBarWidth * 0.25, ScrollBarWidth * 0.08) }
-				.draw(pressed ? ColorF{ 0.85 } : ColorF{ 0.33 });
-			}
-
-			{
-				const bool pressed = ((not m_state.scrollBarGrabbed) && downButton.leftPressed());
-				const Vec2 c = downButton.center();
-
-				if (pressed)
 				{
-					downButton
-						.stretched(-1)
-						.draw(ColorF{ 0.33 });
+					const bool pressed = ((not m_state.scrollBarGrabbed) && downButton.leftPressed());
+					const Vec2 c = downButton.center();
+
+					if (pressed)
+					{
+						downButton
+							.stretched(-1)
+							.draw(ColorF{ 0.33 });
+					}
+
+					Triangle{ c.movedBy(0, ScrollBarWidth * 0.22),
+						c.movedBy(-ScrollBarWidth * 0.25, -ScrollBarWidth * 0.08),
+						c.movedBy(ScrollBarWidth * 0.25, -ScrollBarWidth * 0.08) }
+					.draw(pressed ? ColorF{ 0.85 } : ColorF{ 0.33 });
 				}
 
-				Triangle{ c.movedBy(0, ScrollBarWidth * 0.22),
-					c.movedBy(-ScrollBarWidth * 0.25, -ScrollBarWidth * 0.08),
-					c.movedBy(ScrollBarWidth * 0.25, -ScrollBarWidth * 0.08) }
-				.draw(pressed ? ColorF{ 0.85 } : ColorF{ 0.33 });
+				const RectF innerScrollBarArea = scrollBarArea.stretched(0, -ScrollBarWidth);
+				const double scrollBarHeight = (innerScrollBarArea.h * (static_cast<double>(m_maxLines) / m_state.items.size()));
+				const int32 scrollBarOffset = static_cast<int32>((innerScrollBarArea.h - scrollBarHeight) * (static_cast<double>(m_state.scroll) / (m_state.items.size() - m_maxLines)));
+				const RectF scrollBar(innerScrollBarArea.pos.movedBy(0, (scrollBarOffset - 3.0)), innerScrollBarArea.w, (scrollBarHeight + 6.0));
+
+				scrollBar
+					.stretched(-1, 0)
+					.draw(m_state.scrollBarGrabbed ? ColorF{ 0.33 } : ColorF{ 0.67 });
 			}
-
-			const RectF innerScrollBarArea = scrollBarArea.stretched(0, -ScrollBarWidth);
-			const double scrollBarHeight = (innerScrollBarArea.h * (static_cast<double>(m_maxLines) / m_state.items.size()));
-			const int32 scrollBarOffset = static_cast<int32>((innerScrollBarArea.h - scrollBarHeight) * (static_cast<double>(m_state.scroll) / (m_state.items.size() - m_maxLines)));
-			const RectF scrollBar(innerScrollBarArea.pos.movedBy(0, (scrollBarOffset - 3.0)), innerScrollBarArea.w, (scrollBarHeight + 6.0));
-
-			scrollBar
-				.stretched(-1, 0)
-				.draw(m_state.scrollBarGrabbed ? ColorF{ 0.33 } : ColorF{ 0.67 });
 		}
 	}
-}
 
-RectF Pulldown::getDisplayRegion() const
-{
-	return m_displayRegion;
-}
+	RectF Pulldown::getDisplayRegion() const
+	{
+		return m_displayRegion;
+	}
 
 }
